650bda91480f2d2667395b0ff91b83cfc5a721cf
ilhamfauzi21 <ilham.fauzi.ft21@mail.umy.ac.id>
2025-06-10 15:32:04 +0700

    Menulis baris ke file

--- ioFileWithPath.cpp
+++ ioFileWithPath.cpp
@@ -18,7 +18,10 @@ int main()
     while (true)
     {
         cout << "-";
-        //mendapatkan setiap karakter 
+        //mendapatkan setiap karakter dalam satu baris
+        getline(cin, baris);
+        // loop akan berhenti jika pengguna memasukan nilai dari 'baris' 
     }
-    
+    //Menulis baris ke file
+    outFile << baris << endl;
 }
