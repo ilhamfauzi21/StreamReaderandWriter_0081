508c233f2a87b5f4e79297da3f4ddbcd30c88dc2
ilhamfauzi21 <ilham.fauzi.ft21@mail.umy.ac.id>
2025-06-10 15:15:39 +0700

    untuk obyek exception yang akan digunakan

--- exeptionStandard.cpp
+++ exeptionStandard.cpp
@@ -1,18 +1,4 @@
 #include <iostream>
 #include <exception>
 // untuk obyek exception yang akan digunakan
-#include <array>
-// untuk obyek array yang akan digunakan
-using namespace std;
 
-int main()
-{
-    cout << "Awal Program" << endl; // penanda 1:...
-    try
-    {
-        array<int, 3> data = {10, 20, 30};
-        // pesan array integer 3 elemen
-        cout << data.at(5) << endl;
-        // memanggil elemen ke-5
-    }
-    
