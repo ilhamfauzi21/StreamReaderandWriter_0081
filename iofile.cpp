a2c8c1a34dac8659b3df79cf3696947651d3c63e
ilhamfauzi21 <ilham.fauzi.ft21@mail.umy.ac.id>
2025-06-10 14:55:51 +0700

    input cout

--- iofile.cpp
+++ iofile.cpp
@@ -20,5 +20,10 @@ while (true)
     cout << "-";
     //mendapatkan setiap karakter dalam satu baris 
     getline(cin, baris);
+    //loop akan berhenti jika anda memasukan karakter q
+    if (baris == "q") break;
+    // menulis dan memasukan nilai dari "baris " ke dalam file
+    outfile << baris << endl;
 
 }
+//selesai dalam menulis sekarang tutup filenya
