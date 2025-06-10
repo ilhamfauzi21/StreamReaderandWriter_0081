698d802a99319726c9cebc0a57cb3d1b072e14f3
ilhamfauzi21 <ilham.fauzi.ft21@mail.umy.ac.id>
2025-06-10 15:15:24 +0700

    memanggil elemen ke 5

--- exeptionStandard.cpp
+++ exeptionStandard.cpp
@@ -5,3 +5,14 @@
 // untuk obyek array yang akan digunakan
 using namespace std;
 
+int main()
+{
+    cout << "Awal Program" << endl; // penanda 1:...
+    try
+    {
+        array<int, 3> data = {10, 20, 30};
+        // pesan array integer 3 elemen
+        cout << data.at(5) << endl;
+        // memanggil elemen ke-5
+    }
+    
